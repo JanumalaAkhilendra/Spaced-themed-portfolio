b3972da87ec9607e9e85199386cc4173ad87bfda
Akhilendra <janumalaakhilendra01122000@gmail.com>
2024-11-09 21:32:23 +0530

    first commit

---
+++ src/app/(sub pages)/contact/page.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import bg from "../../../../public/background/S3.avif";
+import Form from "@/components/contact/Form";
+
+export const metadata = {
+  title: "Contact",
+};
+
+export default function Contact() {
+  return (
+    <>
+      <Image
+        src={bg}
+        alt="Next.js Portfolio website's contact page background image"
+        priority
+        sizes="100vw"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-100"
+      />
+
+      <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
+        <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
+          <h1 className="text-accent font-semibold text-center text-4xl capitalize">
+            summon the Astronaut
+          </h1>
+          <p className="text-center font-light text-sm xs:text-base">
+          Embark on a cosmic journey and leave your mark on the digital universe. Whether you're seeking celestial collaborations, exploring the depths of knowledge, or sharing interstellar tales, your messages are like stardust, scattered across the cosmos. Use the form below to send your cosmic communiqué and await the echo of the universe.
+          </p>
+        </div>
+        <Form />
+      </article>
+    </>
+  );
+}
---
+++ src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Copilotkit-weather-bot",
+    description: "A Multipurpose Weather Bot",
+    date: "2024-10-15",
+    demoLink: "https://copilotkit-weather-bot.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "QuickBlox-Chat-Room",
+    description: "A Multi-Purpose Chat Application",
+    date: "2024-10-20",
+    demoLink: "https://github.com/JanumalaAkhilendra/QuickBlox-Chat-Room",
+  },
+  {
+    id: 3,
+    name: "Movie Recommedation Model",
+    description: "A Movie Recommedation System.",
+    date: "2024-06-20",
+    demoLink: "https://github.com/JanumalaAkhilendra/Movie_Recommedation_Model",
+  },
+  {
+    id: 4,
+    name: "Netflix Clone",
+    description: "A Streaming Platform copy webapp ",
+    date: "2024-05-22",
+    demoLink: "https://netflixy-smoky.vercel.app/",
+  },
+  {
+    id: 5,
+    name: "Solasta",
+    description: "A Techfest Event Website",
+    date: "2023-05-15",
+    demoLink: "https://janumalaakhilendra.github.io/solasta/",
+  },
+  {
+    id: 6,
+    name: "Shopper",
+    description: "Your New Shopping App",
+    date: "2023-10-19",
+    demoLink: "https://e-commerce-dun-mu.vercel.app/",
+  },
+  {
+    id: 7,
+    name: "Analog Clock",
+    description: "Creative css Styled Analog Clock",
+    date: "2023-01-8",
+    demoLink: "https://janumalaakhilendra.github.io/Analog_clock/",
+  },
+  {
+    id: 8,
+    name: "Flappy Bird",
+    description: "A 2D game made using Tkinter",
+    date: "2022-11-05",
+    demoLink: "https://github.com/JanumalaAkhilendra/SpotifyClone",
+  },
+  {
+    id: 9,
+    name: "SpotifyClone",
+    description: "A Clone Web-app of Music Player Spotify",
+    date: "2022-06-02",
+    demoLink: "https://github.com/JanumalaAkhilendra/SpotifyClone",
+  },
+  {
+    id: 10,
+    name: "2D-Model TajMahal",
+    description: "2D Model of TajMahal using graphics in Turbo C++",
+    date: "2021-07-03",
+    demoLink: "https://github.com/JanumalaAkhilendra/TajMahal/blob/main/TAJMAHAL.CPP",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/JanumalaAkhilendra",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/akhilendra/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://x.com/Akhilendra_01/",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/Janumala_Akhilendra_resume_01.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+
